33ae98742f5e5970df0462d7b3ad9f923f062d87
danilromas <furiblack904@gmail.com>
2023-06-18 15:16:08 +0300

    15

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" xs="12">
+        <v-carousel>
+          <v-carousel-item v-for="ad in promoAds"
+  		:key="ad.id"
+  		:src="ad.src" cover>
+    <div class="ad-link">
+  	<v-btn class="error" :to="'/ad/' + ad.id">
+  		{{ ad.title }}
+  	</v-btn>
+  </div>
+  </v-carousel-item>
+        </v-carousel>
+      </v-col>
+    </v-row>
+  </v-container>
+  <v-container grid-list-lg>
+    <v-row justify="center">
+      <v-col cols="12" sm="6" md="4" xs="12" v-for="ad in ads" :key="ad.id">
+        <v-card>
+          <v-img :src="ad.src" height="200px"></v-img>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb0">{{ ad.title }}</h3>
+              <div>{{ ad.desc }}</div>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text :to="'/ad/' + ad.id">Open</v-btn>
+            <buy-ad-modal :ad="ad"></buy-ad-modal>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  computed: {
+    promoAds() {
+      return this.$store.getters.promoAds
+    },
+    ads() {
+      return this.$store.getters.ads
+    }
+  }
+}
+</script>
+
+<style scoped>
+.ad-link {
+  position: absolute;
+  bottom: 50px;
+  left: 50%;
+  background: rgb(0, 0, 0, 0.5);
+  transform: translate(-50%, 0);
+  padding: 5px 15px;
+  border-top-right-radius: 5px;
+  border-top-left-radius: 5px;
+}
+</style>

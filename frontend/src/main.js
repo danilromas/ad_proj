ae94daf03516564db36210b92a80e14a827054b3
danilromas <furiblack904@gmail.com>
2023-06-18 15:15:08 +0300

    9

---
+++ frontend/src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import router from './router'
+import store from './store'
+import BuyAdModal from './views/Shared/BuyAdModal'
+
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .component("buy-ad-modal",BuyAdModal)
+  .use(vuetify)
+  .mount('#app')
